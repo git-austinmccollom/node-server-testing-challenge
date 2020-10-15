5709b844ff63f2c4a7e90a9ee9604a6fdd2b2971
git-austinmccollom <austinmccollom@gmail.com>
2020-10-15 16:37:51 -0500

    sanity tests

--- server.spec.js
+++ server.spec.js
@@ -1,6 +1,35 @@
+const { TestScheduler } = require("jest");
 const request = require("supertest");
 const server = require("./server.js");
 
 describe('server.js', () => {
-    describe('index')
+    describe('sanity check', () => {
+        test('get of index route should return ok status', async () => {
+            const expectedStatusCode = 200;
+
+            const response = await request(server).get('/');
+
+            expect(response.status).toEqual(expectedStatusCode);
+
+            // same test using promise .then() instead of async/await
+            // let response;
+            // return request(server).get('/').then(res => {
+            //   response = res;
+
+            //   expect(response.status).toEqual(expectedStatusCode);
+            // })
+        });
+        test('index route response type should be JSON', async () => {
+            const response = await request(server).get('/');
+
+            expect(response.type).toEqual('application/json');
+        })
+        test('index route should return a JSON object', async() => {
+            const expectedBody = { api: "running" };
+
+            const response = await request(server).get('/');
+
+            expect(response.body).toEqual(expectedBody);
+        });
+    })
 })
